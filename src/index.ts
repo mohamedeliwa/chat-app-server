03bc1b7209be54613b5404537321429df2a3584c
mohamedeliwa <mohamed.av11@gmail.com>
2020-10-12 21:25:42 +0200

    initialized io server in a separate file

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,11 @@
 import app from "./app";
-import socketIO from "socket.io";
+import io from './socketIO';
 
 const port: number = 3000;
 
-// app.listen(port, () => {
-//   console.log("Server Started at Port, 3000");
-// });
 
 const server = app.listen(port, () => {
   console.log("listening on port 3000");
 });
 
-const io = socketIO(server);
-
-io.on("connection", (socket: any) => {
-  console.log("a user connected");
-  socket.on("chat message", (msg: any) => {
-    console.log("message: " + msg);
-    io.emit("chat message", msg);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-});
+io.attach(server);
---
+++ src/socketIO.ts
@@ -0,0 +1,19 @@
+import socketIO from "socket.io";
+
+const io = socketIO({
+    // serveClient: false
+});
+
+io.on("connection", (socket: any) => {
+  console.log("a user connected");
+  socket.on("chat message", (msg: any) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+export default io;
