3c3d5f0287f4f6c90d94e617608ade22e149c3a6
mohamedeliwa <mohamed.av11@gmail.com>
2020-10-16 11:20:57 +0200

    creating an array to store the list of connected users, sending it to any user who connects

--- src/socketIO.ts
+++ src/socketIO.ts
@@ -1,11 +1,42 @@
 import socketIO from "socket.io";
 
+/** Types */
+interface User {
+  id: string;
+  name: string;
+}
+/** End of Types */
 const io = socketIO({
-    serveClient: false
+  serveClient: false,
 });
+
+const users: User[] = [];
+
+/**
+ * Events:
+ * "connect"
+ * "chat join"
+ * "chat message"
+ * "disconnect"
+ * "success"
+ * "error"
+ * "update users"
+ */
 
 io.on("connection", (socket) => {
   console.log("a user connected");
+
+  socket.on("chat join", (name) => {
+    console.log(name);
+    console.log(socket.id);
+    users.push({
+      id: socket.id,
+      name,
+    });
+    socket.emit("success", "successfully joined chatroom!");
+    socket.emit("update users", users);
+  });
+
   socket.on("chat message", (msg) => {
     console.log("message: " + msg);
     io.emit("chat message", msg);
